94e1e6c556513c861b12eef9433aac51553462fb
seunghyun <jsh3990@gmail.com>
2026-01-16 02:28:26 +0900

    로그인창 수정중

--- Frontend/src/auth/Auth.js
+++ Frontend/src/auth/Auth.js
@@ -1,5 +1,8 @@
 import React from "react";
 import "./Auth.css";
+import kakaoIcon from "./카카오.png";
+import naverIcon from "./네이버.png";
+import googleIcon from "./구글.png";
 
 const LoginPage = () => {
     const handleLogin = (provider) => {
@@ -11,33 +14,25 @@ const LoginPage = () => {
         <div className="login-container">
             <h1 className="logo">온실 <br/>(로고 사진이면 조켄네)</h1>
 
-            <h3>목표에 집중하는 원격 스터디 그룹</h3>
-            <p>시간과 장소에 구애받지 않는 학습 환경</p>
+            <div className="login-title">
+                <h3>목표에 집중하는 원격 스터디 그룹</h3>
+                <p>시간과 장소에 구애받지 않는 학습 환경</p>
+            </div>
+
 
             <div className="button-group">
-                <button
-                    className="login-button kakao"
-                    onClick={() => handleLogin("kakao")}
-                >
-                    <span className="icon">💬</span>
-                    카카오로 시작하기
-                </button>
+                <a href="#">
+                    <img src={kakaoIcon} className="kakao" alt="카카오 로그인" />
+                </a>
 
-                <button
-                    className="login-button naver"
-                    onClick={() => handleLogin("naver")}
-                >
-                    <span className="icon">N</span>
-                    네이버로 시작하기
-                </button>
+                <a href="##">
+                    <img src={naverIcon} className="naver" alt="네이버 로그인" />
+                </a>
+                <a href="###">
+                    <img src={googleIcon} className="google" alt="구글 로그인" />
+                </a>
 
-                <button
-                    className="login-button google"
-                    onClick={() => handleLogin("google")}
-                >
-                    <span className="icon">G</span>
-                    구글로 시작하기
-                </button>
+
             </div>
 
             <button className="email-login">이메일로 시작하기 &gt;</button>
