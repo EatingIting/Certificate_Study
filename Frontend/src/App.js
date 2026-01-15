2b74310373b316efec2c0393a987bc409139a21b
seunghyun <jsh3990@gmail.com>
2026-01-15 17:50:13 +0900

    스터디 모집방, 로그인 UI 수정중

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,8 +1,16 @@
-import React from "react";
-import MeetingPage from "./webrtc/MeetingPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import RoomPage from "./roomcategorypage/RoomPage";
+import Auth from "./auth/Auth";
 
 function App() {
-  return <MeetingPage />;
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/auth" element={<Auth />} />
+                <Route path="/roompage" element={<RoomPage />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
