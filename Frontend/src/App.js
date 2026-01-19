8a01fcf2f7431bfcfe7407277c2893fcf48227b1
jiyun-youm <yumjiyun1013@gmail.com>
2026-01-19 15:42:40 +0900

    lms

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,8 +1,17 @@
-import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MeetingPage from "./webrtc/MeetingPage";
+import LMSMain from "./lms/LMSMain";
+import LMSSubject from "./lms/LMSSubject"
 
 function App() {
-  return <MeetingPage />;
+  return (
+      <BrowserRouter>
+        <Routes>
+          <Route path="/meeting" element={<MeetingPage />} />
+          <Route path="/lmsMain" element={<LMSMain />} />
+          <Route path="/lms/:id" element={<LMSSubject/>} />
+        </Routes>
+      </BrowserRouter>
+  );
 }
-
 export default App;
---
+++ Frontend/src/lms/assignment/Assignment.js
@@ -0,0 +1,9 @@
+const Assignment=()=>{
+    return(
+        <>
+            과제
+        </>
+    )
+}
+
+export default Assignment;
