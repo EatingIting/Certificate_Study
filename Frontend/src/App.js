f2383fcf8b5486e2f4a0eef2ec2a247fff0b84ee
EatingIting <euijae0611@naver.com>
2026-01-20 15:33:37 +0900

    승현님꺼 pull

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,9 +1,11 @@
-import React from "react";
-import MeetingPage from "./webrtc/MeetingPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import RoomPage from "./roomcategorypage/RoomPage";
+import Auth from "./auth/Auth";
 
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
+<<<<<<< HEAD
   return (
     <BrowserRouter>
       <Routes>
@@ -12,6 +14,16 @@ function App() {
       </Routes>
     </BrowserRouter>
   );
+=======
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/auth" element={<Auth />} />
+                <Route path="/roompage" element={<RoomPage />} />
+            </Routes>
+        </BrowserRouter>
+    );
+>>>>>>> 2b74310 (스터디 모집방, 로그인 UI 수정중)
 }
 
 export default App;
