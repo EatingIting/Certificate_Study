ea212265f77a7b3c77bf02e0f61b3738d5209cfa
EatingIting <euijae0611@naver.com>
2026-01-20 21:50:46 +0900

    cross origin 문제 해결

--- Backend/src/main/java/com/example/demo/controller/AuthController.java
+++ Backend/src/main/java/com/example/demo/controller/AuthController.java
@@ -11,7 +11,7 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*")
 public class AuthController {
 
     private final AuthService authService;
