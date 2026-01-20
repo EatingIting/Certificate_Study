81540533439e53143bbb7cfe7a3a89db2c288df7
EatingIting <euijae0611@naver.com>
2026-01-20 15:35:51 +0900

    merge conflict 해결

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -5,25 +5,16 @@ import Auth from "./auth/Auth";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
-<<<<<<< HEAD
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Navigate to="/room/test" replace />} />
         <Route path="/room/:roomId" element={<MeetingPage />} />
+        <Route path="/auth" element={<Auth />} />
+        <Route path="/roompage" element={<RoomPage />} />
       </Routes>
     </BrowserRouter>
   );
-=======
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/auth" element={<Auth />} />
-                <Route path="/roompage" element={<RoomPage />} />
-            </Routes>
-        </BrowserRouter>
-    );
->>>>>>> 2b74310 (스터디 모집방, 로그인 UI 수정중)
 }
 
 export default App;
