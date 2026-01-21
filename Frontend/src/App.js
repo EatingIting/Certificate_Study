879b852d418c550af37abf0a79ca0fff847c34d0
minkyung <aalsruda12@gamil.com>
2026-01-21 09:45:35 +0900

    mk

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 // import MeetingPage from "./webrtc/MeetingPage";
-// import Main from "./mainpage/Main";
+import Main from "./mainpage/Main";
 import Create from "./roomcreate/Create";
 
 
 function App() {
   // return <MeetingPage />;
     return <Create/>;
+  //   return<Main/>
 }
+
 
 export default App;
