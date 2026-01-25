714f11b4aa90eea7f3034191b46fbe0da32337c5
seunghyun <jsh3990@gmail.com>
2026-01-26 00:52:26 +0900

    oauth 연동

--- Frontend/src/auth/OAuthSuccess.js
+++ Frontend/src/auth/OAuthSuccess.js
@@ -9,12 +9,10 @@ const OAuthSuccess = () => {
 
         if (!token) return;
 
-        // ✅ 토큰 저장
         sessionStorage.setItem("accessToken", token);
 
         alert("로그인 성공!");
 
-        // ✅ 프로필 조회해서 nickname 가져오기
         axios
             .get("http://localhost:8080/api/mypage/me", {
                 headers: {
@@ -23,11 +21,8 @@ const OAuthSuccess = () => {
             })
             .then((res) => {
                 const user = res.data;
-
-                // ✅ nickname 저장 (여기서!)
                 sessionStorage.setItem("nickname", user.nickname);
 
-                // OAuth 유저 정보 부족하면 마이페이지로 이동
                 if (!user.birthDate || !user.gender) {
                     alert(
                         "원활한 스터디 가입을 위해 회원 정보를 마이페이지에서 수정해주세요."
